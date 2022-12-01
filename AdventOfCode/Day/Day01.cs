c538e35c738c7dadc391c3c2114d3d8f1b56e0f0
JonnaJK <96888686+JonnaJK@users.noreply.github.com>
2022-12-01 15:59:33 +0100

    Add project files.

---
+++ AdventOfCode/Day/Day01.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day
+{
+    internal class Day01
+    {
+        private readonly string _path;
+
+        public Day01(string path)
+        {
+            _path = Path.Combine(path, $"Input{this.GetType().Name}.txt");
+            PartOne();
+            PartTwo();
+        }
+
+        public void PartOne()
+        {
+
+        }
+        public void PartTwo()
+        {
+
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode.Day;
+
+namespace AdventOfCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(Path.GetFullPath(@"..\..\..\"), "Inputs");
+            _ = new Day01(path);
+            //_ = new Day02(path);
+
+            Console.ReadLine();
+        }
+    }
+}
