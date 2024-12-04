3c04875f4f36bd562c579e58f0af8d7d3ac1e464
jonna <jonna.frida.karlsson@gmail.com>
2024-12-04 09:11:51 +0100

    Added new project with advent of code 2024 and made the architecture

---
+++ AdventOfCode2024/Day/Day01.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode2024.Day;
+
+public class Day01
+{
+    private readonly string _path;
+
+    public Day01(string path)
+    {
+        _path = Path.Combine(path, $"Input{GetType().Name}.txt");
+        PartOne();
+        PartTwo();
+    }
+
+    public void PartOne()
+    {
+        var text = File.ReadAllText(_path);
+
+
+
+        Console.WriteLine("Part one: " + "");
+    }
+
+    public void PartTwo()
+    {
+        var text = File.ReadAllText(_path);
+
+        Console.WriteLine("Part two: " + "");
+    }
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,7 @@
+﻿using AdventOfCode2024.Day;
+
+var path = Path.Combine(Path.GetFullPath(@"..\..\..\"), "Inputs");
+
+_ = new Day01(path);
+
+Console.ReadLine();
