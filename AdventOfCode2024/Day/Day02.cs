70403d49760f03824e93672e68a7d5130e2ae23e
jonna <jonna.frida.karlsson@gmail.com>
2024-12-06 22:13:37 +0100

    Finished day 2 part 2

--- AdventOfCode2024/Day/Day02.cs
+++ AdventOfCode2024/Day/Day02.cs
@@ -23,13 +23,13 @@ public class Day02
         var sum = 0;
         foreach (var report in reports)
         {
-            sum += IsSafeReport(report);
+            sum += IsSafeReport(report, false);
         }
 
         Console.WriteLine("Part one: " + sum);
     }
 
-    private static int IsSafeReport(List<int> report)
+    private static int IsSafeReport(List<int> report, bool isUsingProblemDampener)
     {
         var isAscending = (report[0] - report[1]) < 0;
 
@@ -38,23 +38,51 @@ public class Day02
             var diff = report[i] - report[i + 1];
 
             if (Math.Abs(diff) > 3 || diff == 0) // Unsafe - To much or no in-/decrease
-                return 0;
+            {
+                if (isUsingProblemDampener is false)
+                    return 0;
+
+                return IsSafeReportWithProblemDampener(report, i);
+            }
             if ((isAscending ^ diff < 0)) // Unsafe - report is increasing and decreasing
-                return 0;
+            {
+                if (isUsingProblemDampener is false)
+                    return 0;
+
+                return IsSafeReportWithProblemDampener(report, i);
+            }
         }
         return 1;
     }
 
     private void PartTwo()
     {
-        var text = File.ReadAllLines(_path)
+        var reports = File.ReadAllLines(_path)
             .Select(x =>
                 x.Split(' ', StringSplitOptions.RemoveEmptyEntries)
                 .Select(y => int.Parse(y))
                 .ToList())
             .ToList();
 
+        var sum = 0;
+        foreach (var report in reports)
+        {
+            sum += IsSafeReport(report, true);
+        }
 
-        Console.WriteLine("Part one: " + "sum");
+        Console.WriteLine("Part one: " + sum);
+    }
+
+    private static int IsSafeReportWithProblemDampener(List<int> report, int i)
+    {
+        var start = Math.Max(0, i - 1);
+        for (int j = start; j <= i + 1; j++)
+        {
+            var newReport = report.Select(x => x).ToList();
+            newReport.RemoveAt(j);
+            if (IsSafeReport(newReport, false) is 1)
+                return 1;
+        }
+        return 0;
     }
 }
