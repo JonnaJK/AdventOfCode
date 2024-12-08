7a0eea2ce87cebe1e9de235f86187552574f0902
Jonna <Jonna@DESKTOP-D4B7IKS>
2024-12-08 15:22:38 +0100

    Added day 4

---
+++ AdventOfCode2024/Day/Day04.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode2024.Day;
+
+public class Day04
+{
+    private readonly string _path;
+    public Day04(string path)
+    {
+        _path = Path.Combine(path, $"Input{GetType().Name}.txt");
+        PartOne();
+        PartTwo();
+    }
+
+    private void PartOne()
+    {
+        var reports = File.ReadAllText(_path);
+
+        Console.WriteLine("Part one: " + "sum");
+    }
+
+    private void PartTwo()
+    {
+        var reports = File.ReadAllText(_path);
+
+        Console.WriteLine("Part two: " + "sum");
+    }
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -4,6 +4,7 @@ var path = Path.Combine(Path.GetFullPath(@"..\..\..\"), "Inputs");
 
 //_ = new Day01(path);
 //_ = new Day02(path);
-_ = new Day03(path);
+//_ = new Day03(path);
+_ = new Day04(path);
 
 Console.ReadLine();
