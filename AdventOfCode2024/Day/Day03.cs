170995362c4726bece25886886916e34cf4eb84a
Jonna <Jonna@DESKTOP-D4B7IKS>
2024-12-08 14:55:21 +0100

    finished day 3 part 2

--- AdventOfCode2024/Day/Day03.cs
+++ AdventOfCode2024/Day/Day03.cs
@@ -45,6 +45,70 @@ public class Day03
     {
         var reports = File.ReadAllText(_path);
 
-        Console.WriteLine("Part two: " + "sum");
+        var dontPattern = "don't()";
+        var doPattern = "do()";
+        //var dontPattern = new Regex("don't\\(\\)");
+
+        var splittedByDont = reports.Split(new string[] { dontPattern.ToString() }, StringSplitOptions.RemoveEmptyEntries);
+
+        var pattern = new Regex("mul\\([0-9]+,[0-9]+\\)");
+        var doOneTime = true;
+        var sum = 0;
+        for (int i = 0; i < splittedByDont.Length; i++)
+        {
+            // do the first always
+            if (doOneTime)
+            {
+                var matches = pattern.Matches(splittedByDont[i]).ToList();
+
+                foreach (var match in matches)
+                {
+                    var reducesText = match.Value.ToString().Substring(4);
+                    var delimitor = reducesText.IndexOf(',');
+
+                    var firstValue = int.Parse(reducesText[0..delimitor]);
+                    var secondValues = reducesText[(delimitor + 1)..].SkipLast(1);
+
+                    var second = string.Empty;
+                    foreach (var value in secondValues)
+                    {
+                        second += value;
+                    }
+                    var resultPerMul = firstValue * int.Parse(second);
+                    sum += resultPerMul;
+                }
+                doOneTime = false;
+                continue;
+            }
+
+            var stringContainsDo = splittedByDont[i].Contains(doPattern);
+            if (stringContainsDo is false)
+                continue;
+
+            var dos = splittedByDont[i].Split(new string[] { doPattern }, StringSplitOptions.RemoveEmptyEntries).Skip(1);
+            foreach (var item in dos)
+            {
+                var matches = pattern.Matches(item).ToList();
+
+                foreach (var match in matches)
+                {
+                    var reducesText = match.Value.ToString().Substring(4);
+                    var delimitor = reducesText.IndexOf(',');
+
+                    var firstValue = int.Parse(reducesText[0..delimitor]);
+                    var secondValues = reducesText[(delimitor + 1)..].SkipLast(1);
+
+                    var second = string.Empty;
+                    foreach (var value in secondValues)
+                    {
+                        second += value;
+                    }
+                    var resultPerMul = firstValue * int.Parse(second);
+                    sum += resultPerMul;
+                }
+            }
+        }
+
+        Console.WriteLine("Part two: " + sum);
     }
 }
