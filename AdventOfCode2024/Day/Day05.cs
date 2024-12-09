24bf5609f183907e374dd8e61c3f0b7bfcbd863a
Jonna <Jonna@DESKTOP-D4B7IKS>
2024-12-09 21:03:36 +0100

    Finished day 5 part 1

--- AdventOfCode2024/Day/Day05.cs
+++ AdventOfCode2024/Day/Day05.cs
@@ -12,9 +12,39 @@ public class Day05
 
     private void PartOne()
     {
-        var text = File.ReadAllLines(_path);
+        var input = File.ReadAllLines(_path);
 
-        Console.WriteLine("Part one: " + "sum");
+        var rules = input.TakeWhile(x => x != "").Select(x => x.Split('|').ToList()).ToList();
+        var updates = input.TakeLast(input.Length - rules.Count - 1).ToList();
+
+        var correctlyOrderedUpdates = new List<string>();
+        foreach (var update in updates)
+        {
+            var pages = update.Split(',').ToList();
+            for (int i = 0; i < pages.Count; i++)
+            {
+                var page = pages[i]; //75
+                var ruleForUpdate = rules.Where(x => x.First().Contains(page)).Select(x => x.Last()).ToList(); // eg 15
+
+                var pagesBefore = pages[..pages.IndexOf(page)];
+
+                if (ruleForUpdate.Any(pagesBefore.Contains))
+                    break;
+
+                if (i == pages.Count - 1)
+                    correctlyOrderedUpdates.Add(update);
+            }
+        }
+
+        var sum = 0;
+        foreach (var update in correctlyOrderedUpdates)
+        {
+            var asd = update.Split(',');
+            var asdasd = int.Parse(asd[asd.Length / 2]);
+            sum += asdasd;
+        }
+
+        Console.WriteLine("Part one: " + sum);
     }
 
     private void PartTwo()
