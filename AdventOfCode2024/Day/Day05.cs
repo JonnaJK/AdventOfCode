73ad239cf1b87ccaa6f2a123cd2b4fb1641deb38
jonna <jonna.frida.karlsson@gmail.com>
2024-12-09 15:30:19 +0100

    Added day 5

---
+++ AdventOfCode2024/Day/Day05.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode2024.Day;
+
+public class Day05
+{
+    private readonly string _path;
+    public Day05(string path)
+    {
+        _path = Path.Combine(path, $"Input{GetType().Name}.txt");
+        PartOne();
+        PartTwo();
+    }
+
+    private void PartOne()
+    {
+        var text = File.ReadAllLines(_path);
+
+        Console.WriteLine("Part one: " + "sum");
+    }
+
+    private void PartTwo()
+    {
+        var text = File.ReadAllLines(_path);
+
+        Console.WriteLine("Part two: " + "sum");
+    }
+}
