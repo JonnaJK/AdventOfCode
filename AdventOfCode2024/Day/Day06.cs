f74b313964a5841f6a74997126be0427f3ce4772
Jonna <Jonna@DESKTOP-D4B7IKS>
2024-12-22 15:50:31 +0100

    wip

--- AdventOfCode2024/Day/Day06.cs
+++ AdventOfCode2024/Day/Day06.cs
@@ -14,30 +14,63 @@ public class Day06
     {
         var input = File.ReadAllLines(_path);
 
-        var startingDirection = '^';
+        var direction = '^';
         if (input.Any(x => x.Contains('^')))
-            startingDirection = '^';
+            direction = '^';
         else if (input.Any(x => x.Contains('v')))
-            startingDirection = 'v';
+            direction = 'v';
         else if (input.Any(x => x.Contains('<')))
-            startingDirection = '<';
+            direction = '<';
         else if (input.Any(x => x.Contains('>')))
-            startingDirection = '>';
+            direction = '>';
 
         (int x, int y) guardCordinates =
         (
-            input.ToList().FindIndex(x => x.Contains(startingDirection)),
-            Array.IndexOf(input.First(x => x.Contains(startingDirection)).ToCharArray(), startingDirection)
+            input.ToList().FindIndex(x => x.Contains(direction)),
+            Array.IndexOf(input.First(x => x.Contains(direction)).ToCharArray(), direction)
         );
 
+        // Continue while the guard is in map
         while (input.Any(x => x.Contains('^')) ||
             input.Any(x => x.Contains('v')) ||
             input.Any(x => x.Contains('<')) ||
             input.Any(x => x.Contains('>')))
         {
-            for (int i = 0; i < input.Length; i++)
+            for (int i = guardCordinates.x; i < input.Length; i++)
             {
+                for (int j = guardCordinates.y; j < input[1].Length; j++)
+                {
+                    switch (direction)
+                    {
+                        case '^':
+                            if (i - 1 < 0)
+                                break;
+                            var nextTile = input[i - 1][j];
+                            if (nextTile == '.')
+                            {
+                                guardCordinates = (i - 1, j);
+                                //input[i - 1][j] = 'X';
+                            }
+                            else if (nextTile == '#')
+                            {
 
+                            }
+                            else if (nextTile == 'X')
+                            {
+
+                            }
+                            break;
+                        case 'v':
+                            break;
+                        case '<':
+                            break;
+                        case '>':
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
             }
         }
 
