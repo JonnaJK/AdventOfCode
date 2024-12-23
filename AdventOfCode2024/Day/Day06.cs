26849c8d1248b143e097938f8df1ef0675a3bb33
Jonna <Jonna@DESKTOP-D4B7IKS>
2024-12-23 12:27:36 +0100

    WIP

--- AdventOfCode2024/Day/Day06.cs
+++ AdventOfCode2024/Day/Day06.cs
@@ -10,9 +10,19 @@ public class Day06
         PartTwo();
     }
 
+    private static (int x, int y) GetDirection(char input) => input switch
+    {
+        '^' => (0, -1),
+        'v' => (0, 1),
+        '<' => (-1, 0),
+        '>' => (1, 0),
+        _ => throw new NotImplementedException()
+    };
+
     private void PartOne()
     {
         var input = File.ReadAllLines(_path);
+        var map = ConvertToMatrix(input);
 
         var direction = '^';
         if (input.Any(x => x.Contains('^')))
@@ -30,41 +40,103 @@ public class Day06
             Array.IndexOf(input.First(x => x.Contains(direction)).ToCharArray(), direction)
         );
 
+
         // Continue while the guard is in map
-        while (input.Any(x => x.Contains('^')) ||
-            input.Any(x => x.Contains('v')) ||
-            input.Any(x => x.Contains('<')) ||
-            input.Any(x => x.Contains('>')))
+        //while (input.Any(x => x.Contains('^')) ||
+        //    input.Any(x => x.Contains('v')) ||
+        //    input.Any(x => x.Contains('<')) ||
+        //    input.Any(x => x.Contains('>')))
+        //{
+        var guardIsInMap = true;
+        while (guardIsInMap)
         {
-            for (int i = guardCordinates.x; i < input.Length; i++)
+            for (int i = guardCordinates.y; i < map.Length; i++)
             {
-                for (int j = guardCordinates.y; j < input[1].Length; j++)
+                if (guardIsInMap is false)
+                    break;
+
+                for (int j = guardCordinates.x; j < map.Length; j++)
                 {
+                    if (guardIsInMap is false)
+                        break;
+
+                    if (map[i, j] != direction.ToString())
+                        continue;
+
                     switch (direction)
                     {
                         case '^':
                             if (i - 1 < 0)
+                            {
+                                guardIsInMap = false;
                                 break;
-                            var nextTile = input[i - 1][j];
-                            if (nextTile == '.')
+                            }
+
+                            var nextTile = map[i - 1, j];
+                            if (nextTile == "." || nextTile == "X")
                             {
                                 guardCordinates = (i - 1, j);
-                                //input[i - 1][j] = 'X';
+                                map[i - 1, j] = "^";
+                                map[i, j] = "X";
                             }
-                            else if (nextTile == '#')
+                            else if (nextTile == "#")
                             {
-
+                                direction = '>';
                             }
-                            else if (nextTile == 'X')
-                            {
-
-                            }
+                            //else if (nextTile == "X")
+                            //{
+                            //    guardCordinates = (i - 1, j);
+                            //    map[i - 1, j] = "^";
+                            //    map[i, j] = "X";
+                            //}
                             break;
                         case 'v':
+                            if (i + 1 > map.Length)
+                                break;
+
+                            var nextTile2 = map[i + 1, j];
+                            if (nextTile2 == "." || nextTile2 == "X")
+                            {
+                                guardCordinates = (i + 1, j);
+                                map[i + 1, j] = "v";
+                                map[i, j] = "X";
+                            }
+                            else if (nextTile2 == "#")
+                            {
+                                direction = '<';
+                            }
                             break;
                         case '<':
+                            if (j - 1 < 0)
+                                break;
+
+                            var nextTile3 = map[i, j - 1];
+                            if (nextTile3 == "." || nextTile3 == "X")
+                            {
+                                guardCordinates = (i, j - 1);
+                                map[i, j - 1] = "<";
+                                map[i, j] = "X";
+                            }
+                            else if (nextTile3 == "#")
+                            {
+                                direction = '^';
+                            }
                             break;
                         case '>':
+                            if (j + 1 > map.Length)
+                                break;
+
+                            var nextTile4 = map[i, j + 1];
+                            if (nextTile4 == "." || nextTile4 == "X")
+                            {
+                                guardCordinates = (i, j + 1);
+                                map[i, j + 1] = ">";
+                                map[i, j] = "X";
+                            }
+                            else if (nextTile4 == "#")
+                            {
+                                direction = 'v';
+                            }
                             break;
                         default:
                             break;
@@ -88,6 +160,20 @@ public class Day06
          */
 
         Console.WriteLine("Part one: " + "sum");
+    }
+
+    private static string[,] ConvertToMatrix(string[] reports)
+    {
+        var matrix = new string[reports.Length, reports[1].Length];
+
+        for (int i = 0; i < reports.Length; i++)
+        {
+            for (int j = 0; j < reports[0].Length; j++)
+            {
+                matrix[i, j] = reports[i][j].ToString();
+            }
+        }
+        return matrix;
     }
 
     private void PartTwo()
